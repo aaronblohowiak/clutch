28a88fe866fef7e83eae96c8ddbe44c5b5673e27
Aaron P Blohowiak <aaron.blohowiak@gmail.com>
2013-02-26 23:38:39 -0800

    Initial Commit

---
+++ clutch/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  clutch
+//
+//  Created by Aaron Blohowiak on 2/26/13.
+//  Copyright (c) 2013 Aaron Blohowiak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
